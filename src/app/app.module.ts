ee919ac7c578a7d22e45e1f7115336fd8b47b523
diegosanfar <diegosantosfaria@hotmail.com>
2021-09-08 19:24:56 -0300

    teste

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID, DEFAULT_CURRENCY_CODE } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 
+import { ConfigComponent } from './config/config.component';
+import localePt from '@angular/common/locales/pt';
+import { TimerComponent } from './timer/timer.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localePt);
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent, ConfigComponent, TimerComponent],
+  imports: [BrowserModule, ReactiveFormsModule],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
+    { provide: DEFAULT_CURRENCY_CODE, useValue: 'USD' },
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
